91f9e6294f867dac553bf8bd98e593cb86d16032
Bhisca <94308088+Bhisca@users.noreply.github.com>
2023-07-31 21:50:11 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,37 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#add-entry-form').addEventListener('submit', submitForm);
-});
+  
+  const addNewEntryBtn = document.getElementById('add-new-entry-btn');
+  const popupOverlay = document.getElementById('popup-overlay');
+  const popupForm = document.getElementById('popup-form');
+  const closeBtn = document.getElementById('close-btn');
+  const form = document.getElementById('add-entry-form');
 
-function submitForm(event) {
-  event.preventDefault();
+  addNewEntryBtn.addEventListener('click', function() {
+    popupOverlay.style.display = 'block';
+    popupForm.style.display = 'block';
+  });
 
-  const name = document.querySelector('#name').value;
-  const platform = document.querySelector('#platform').value;
-  const status = document.querySelector('#status').value;
-  const date = document.querySelector('#date_added').value;
-  const comment = document.querySelector('#comment').value;
+  closeBtn.addEventListener('click', function() {
+    popupOverlay.style.display = 'none';
+    popupForm.style.display = 'none';
+  });
 
-  fetch('https://allowapps.onrender.com/insert', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name,
-      platform,
-      status,
-      date_added: date,
-      comment,
-    }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        alert('Entry added successfully');
-        window.location.href = 'home.html';
-      } else {
-        alert('Error adding entry');
-      }
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.querySelector('#name').value;
+    const platform = document.querySelector('#platform').value;
+    const status = document.querySelector('#status').value;
+    const date = document.querySelector('#date_added').value;
+    const comment = document.querySelector('#comment').value;
+
+    fetch('https://allowapps.onrender.com/insert', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        platform,
+        status,
+        date_added: date,
+        comment,
+      }),
     })
-    .catch(err => console.log(err));
-}
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          alert('Entry added successfully');
+          popupOverlay.style.display = 'none';
+          popupForm.style.display = 'none';
+          window.location.href = 'home.html';
+        } else {
+          alert('Error adding entry');
+        }
+      })
+      .catch(err => console.log(err));
+  });
+});
